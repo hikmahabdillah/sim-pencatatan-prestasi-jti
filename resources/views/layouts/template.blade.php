77149d2f2dc90fc33b265ed74745aab09dd49f08
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-04-30 20:21:26 +0700

    setup layouting

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('argon/assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('argon/assets/img/favicon.png') }}">
+    <title>Talenti | Talenta JTI</title>
+
+    <!-- Fonts and icons -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('argon/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('argon/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <!-- Argon CSS -->
+    <link id="pagestyle" href="{{ asset('argon/assets/css/argon-dashboard.css') }}" rel="stylesheet" />
+
+    {{-- @vite('resources/css/app.css') --}}
+    {{-- @vite('resources/js/app.js') --}}
+</head>
+
+<body class="{{ $class ?? '' }}">
+
+    <div class="min-height-300 bg-primary position-absolute w-100"></div>
+    <div class="position-absolute w-100 min-height-300 top-0"
+        style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/profile-layout-header.jpg'); background-position-y: 50%;">
+        <span class="mask bg-primary opacity-6"></span>
+    </div>
+
+    @include('layouts.sidebar')
+
+    <main class="main-content border-radius-lg">
+        @yield('content')
+    </main>
+
+    {{-- @include('components.fixed-plugin') --}}
+
+    <!-- Core JS Files -->
+    <script src="{{ asset('argon/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('argon/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('argon/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('argon/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <!-- Argon Dashboard JS -->
+    <script src="{{ asset('argon/assets/js/argon-dashboard.js') }}"></script>
+
+    @stack('js')
+</body>
+
+</html>
