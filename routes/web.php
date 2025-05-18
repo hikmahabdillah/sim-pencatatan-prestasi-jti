fb37f4cb863acc45374417cebe963a9a2162b1ac
Anandarahmawati24 <144120040+Anandarahmawati24@users.noreply.github.com>
2025-05-18 12:16:20 +0700

    add controller, model and route for kategori

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +28,14 @@ Route::get('/modal', function () {
 
 Route::get('/login', function () {
     return view('auth.login');
+});
+
+Route::prefix('kategori')->group(function () {
+Route::get('/', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
+Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
+Route::post('/', [KategoriController::class, 'store_ajax'])->name('kategori.store_ajax');
+Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax'])->name('kategori.edit_ajax');
+Route::put('/{id}', [KategoriController::class, 'update_ajax'])->name('kategori.update_ajax');
+Route::delete('/{id}/delete_ajax', [KategoriController::class, 'destroy'])->name('kategori.destroy');
 });
