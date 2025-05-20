4b01c68b131d3085e09b16b38acc81b2e3a6a97e
HaQi <luqmanananta745@gmail.com>
2025-05-20 16:24:03 +0700

    Add CRUD for Tingkat Prestasi Table

---
+++ app/Models/TingkatPrestasiModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TingkatPrestasiModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tingkat_prestasi';
+    protected $primaryKey = 'id_tingkat_prestasi';
+    protected $fillable = ['nama_tingkat_prestasi'];
+}
