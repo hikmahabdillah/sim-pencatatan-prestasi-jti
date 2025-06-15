b652c941eab17dadfa39d3a0c4be40d16f03235e
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-06-15 16:12:26 +0700

    fix bug in navbar function notification

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -180,7 +180,7 @@ class DashboardController extends Controller
         $rankMahasiswaByPrestasi = $this->rankMahasiswaByPrestasi();
 
         // Notif
-        $allNotifikasi = auth()->user()->notifications->latest()->limit(10)->get();
+        $allNotifikasi = auth()->user()->notifications()->latest()->limit(10)->get();
 
         return view('dashboard', [
             'breadcrumb' => $breadcrumb,
