ded62a77f88f5a158afc3ffa34743f3f8eea79b2
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-06-17 11:29:46 +0700

    fix seeder for rekomendasi feature

--- database/seeders/Kategori_lomba_pivotSeeder.php
+++ database/seeders/Kategori_lomba_pivotSeeder.php
@@ -15,22 +15,22 @@ class Kategori_lomba_pivotSeeder extends Seeder
         $timestamp = now();
 
         DB::table('kategori_lomba_pivot')->insert([
-            ['id_lomba' => 1, 'id_kategori' => 10, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 1, 'id_kategori' => 16, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 1, 'id_kategori' => 17, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 2, 'id_kategori' => 12, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 2, 'id_kategori' => 11, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 3, 'id_kategori' => 13, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 3, 'id_kategori' => 19, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 4, 'id_kategori' => 14, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 4, 'id_kategori' => 23, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 4, 'id_kategori' => 22, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 5, 'id_kategori' => 16, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 5, 'id_kategori' => 17, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 5, 'id_kategori' => 11, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 6, 'id_kategori' => 15, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 6, 'id_kategori' => 23, 'created_at' => $timestamp, 'updated_at' => $timestamp],
-            ['id_lomba' => 6, 'id_kategori' => 10, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 1, 'id_kategori' => 8, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 1, 'id_kategori' => 14, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 1, 'id_kategori' => 15, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 2, 'id_kategori' => 9, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 2, 'id_kategori' => 10, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 3, 'id_kategori' => 11, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 3, 'id_kategori' => 17, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 4, 'id_kategori' => 12, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 4, 'id_kategori' => 20, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 4, 'id_kategori' => 11, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 5, 'id_kategori' => 8, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 5, 'id_kategori' => 14, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 5, 'id_kategori' => 15, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 6, 'id_kategori' => 8, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 6, 'id_kategori' => 21, 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['id_lomba' => 6, 'id_kategori' => 13, 'created_at' => $timestamp, 'updated_at' => $timestamp],
         ]);
     }
 }
